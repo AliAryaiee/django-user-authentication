586272decef33d6a308e8e6a02a7ef339443c4a9
AliAryaie <a.aryaienejad@mail.sbu.ac.ir>
2022-09-19 08:54:26 +0430

    JWT Configured for Retrieving User Data

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 from . import views
 
 
 urlpatterns = [
-    path("register/", views.UserRegister.as_view())
+    path("register/", views.UserRegister.as_view()),
+    path("user/<str:username>/", views.UserProfile.as_view()),
+    path("token/", TokenObtainPairView.as_view()),
+    path("token/refresh/", TokenRefreshView.as_view()),
+    path("api/token/verify/", TokenVerifyView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -23,3 +23,34 @@ class UserRegister(APIView):
                 password=validated_data["password"]
             )
             return Response(serialized_user.data)
+
+
+class UserProfile(APIView):
+    """
+        Retrieve User Class View
+    """
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_user_object(self, username: str):
+        """
+            Find User by Username
+        """
+        try:
+            return serializers.User.objects.get(username=username)
+        except serializers.User.DoesNotExist:
+            return None
+
+    def get(self, request, username: str):
+        """
+            GET Request
+        """
+        db_user = self.get_user_object(username=username)
+        if db_user:
+            serialized_data = serializers.UserRegisterSerializer(
+                instance=db_user
+            )
+            return Response(serialized_data.data, status=status.HTTP_200_OK)
+        Response(
+            {"response": f"There Is Not User by Username ({username})"},
+            status=status.HTTP_400_BAD_REQUEST
+        )
